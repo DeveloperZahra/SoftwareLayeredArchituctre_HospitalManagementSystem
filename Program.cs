e313681747ad46a09491333bbeaaf8aa7e23efd5
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-05 15:53:24 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SoftwareLayeredArchituctre_HospitalManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
