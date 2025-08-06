4253fe55201bba150d52f04dd6168c18114757f0
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 16:07:54 +0400

    This migration creates the Doctors table and sets up the initial database schema.

---
+++ Migrations/20250806115642_initilcreation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class initilcreation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doctors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HolderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Specialty = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doctors", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doctors");
+        }
+    }
+}
