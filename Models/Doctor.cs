d4b6fec767f1fadc31bae736656a3a90edf239ac
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 14:40:11 +0400

    Define the Doctor class to represent a doctor entity in the system

---
+++ Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Models
+{
+    // Define the Doctor class to represent a doctor entity in the system
+    public class Doctor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string HolderName { get; set; }
+
+        public string Specialty { get; set; }
+
+    }
+}
