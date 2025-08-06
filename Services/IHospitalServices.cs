825db427049d77f99869ff1f87001670802debb7
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 15:26:41 +0400

    Interface defining the service layer contract for managing doctor-related operations

---
+++ Services/IHospitalServices.cs
@@ -0,0 +1,11 @@
+﻿using SoftwareLayeredArchituctre_HospitalManagementSystem.Models;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Services
+{
+    // Interface defining the service layer contract for managing doctor-related operations
+    internal interface IHospitalServices
+    {
+        void CreateAccount(string holdername, string DSpecialty);
+        IEnumerable<Doctor> GetAllDoctors();
+    }
+}
