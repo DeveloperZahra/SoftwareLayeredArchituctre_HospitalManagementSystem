ed645ea01f01038ca48daff4e717d3f2de182fb2
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 15:12:14 +0400

    Retrieves a list of all doctors from the database

--- Repositories/IHospitalRepo.cs
+++ Repositories/IHospitalRepo.cs
@@ -8,7 +8,7 @@ namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Repositories
         void Add(Doctor doctor); // Adds a new doctor to the database
         void Delete(int id); // Deletes a doctor from the database using their ID
         Doctor GetById(int id); // Retrieves a doctor by their unique ID
-        List<Doctor> GetDoctors();
+        List<Doctor> GetDoctors();  // Retrieves a list of all doctors from the database
         void Update(Doctor doctor);
     }
 }
