be77da9913df90296d879a887fa2d20f7586184e
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 15:16:58 +0400

    HospitalServices implements IHospitalServices and provides higher-level business logic for doctor operations

---
+++ Services/HospitalServices.cs
@@ -0,0 +1,44 @@
+﻿using SoftwareLayeredArchituctre_HospitalManagementSystem.Models;
+using SoftwareLayeredArchituctre_HospitalManagementSystem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Services
+{
+    // HospitalServices implements IHospitalServices and provides higher-level business logic for doctor operations
+    internal class HospitalServices : IHospitalServices
+    {
+        private readonly IHospitalRepo _repo;
+
+        public HospitalServices(IHospitalRepo repo)
+        {
+            _repo = repo;
+
+        }
+
+        public void CreateAccount(string holdername, string DSpecialty)
+        {
+            var abc = new Doctor
+            {
+                HolderName = holdername,
+                Specialty = DSpecialty
+            };
+        }
+
+        public IEnumerable<Doctor> GetAllDoctors()
+
+        {
+            return _repo.GetDoctors();
+        }
+
+
+
+
+
+
+
+    }
+}
