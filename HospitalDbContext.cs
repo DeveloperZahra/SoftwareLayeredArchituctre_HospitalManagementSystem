695b077d5e7883f66b42997e732dde227aaf5ddb
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 15:28:44 +0400

    Defines the database context for the Hospital Management System, enabling interaction with the database using Entity Framework Core

---
+++ HospitalDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftwareLayeredArchituctre_HospitalManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem
+{
+    // Defines the database context for the Hospital Management System,
+    // enabling interaction with the database using Entity Framework Core
+    internal class HospitalDbContext: DbContext
+    {
+        public DbSet<Doctor> Doctors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local); Initial Catalog=HospitalDb; Integrated Security=True; TrustServerCertificate=True");
+
+
+        }
+    }
+}
