0bc65d939d577373d662658d5c2a1be1bbd7c578
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 14:46:58 +0400

    HospitalRepo implements IHospitalRepo interface and handles CRUD operations for Doctor entities

---
+++ Repositories/HospitalRepo.cs
@@ -0,0 +1,56 @@
+﻿using SoftwareLayeredArchituctre_HospitalManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Repositories
+{
+    // HospitalRepo implements IHospitalRepo interface and handles CRUD operations for Doctor entities
+    class HospitalRepo : IHospitalRepo
+    {
+        private readonly HospitalDbContext _context;
+
+
+        public HospitalRepo(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+        }
+
+        public List<Doctor> GetDoctors()
+        {
+            return _context.Doctors.ToList();
+
+        }
+
+        public Doctor GetById(int id)
+        {
+            return _context.Doctors.Find(id);
+        }
+
+        public void Update(Doctor doctor)
+        {
+            _context.Doctors.Update(doctor);
+            _context.SaveChanges();
+        }
+
+
+
+        public void Delete(int id)
+        {
+            var abc = GetById(id);
+            if (abc != null)
+            {
+                _context.Doctors.Remove(abc);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Repositories/IHospitalRepo.cs
@@ -0,0 +1,13 @@
+﻿using SoftwareLayeredArchituctre_HospitalManagementSystem.Models;
+
+namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Repositories
+{
+    public interface IHospitalRepo
+    {
+        void Add(Doctor doctor);
+        void Delete(int id);
+        Doctor GetById(int id);
+        List<Doctor> GetDoctors();
+        void Update(Doctor doctor);
+    }
+}
