9ac2437db74a473471ca873340a83e57d16f24cb
Zahra Alhabsi <alhabsi01994@gmail.com>
2025-08-06 15:09:59 +0400

    Adds a new doctor to the database

--- Repositories/IHospitalRepo.cs
+++ Repositories/IHospitalRepo.cs
@@ -5,7 +5,7 @@ namespace SoftwareLayeredArchituctre_HospitalManagementSystem.Repositories
     // Interface defining contract for Doctor repository operations
     public interface IHospitalRepo
     {
-        void Add(Doctor doctor);
+        void Add(Doctor doctor); // Adds a new doctor to the database
         void Delete(int id);
         Doctor GetById(int id);
         List<Doctor> GetDoctors();
